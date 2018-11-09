4f8e8da86a1d442096d65e6a529b576a19e17f02
Ilya Glushchuk <musicguy675@gmail.com>
2018-11-09 21:40:39 +1000

    создал файл с константами

---
+++ VK Mobile Challenge/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  VK Mobile Challenge
+//
+//  Created by Ilya Gluschuk on 09/11/2018.
+//  Copyright © 2018 Ilya Glushchuk. All rights reserved.
+//
+
+import UIKit
+
+enum Constants {
+    static let appId = "6746477"
+}
