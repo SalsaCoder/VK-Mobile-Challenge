c90288e7507c77b13e578955e4194b10360380df
Ilya Glushchuk <musicguy675@gmail.com>
2018-11-09 20:40:53 +1000

    Завел модель пользователя

---
+++ VK Mobile Challenge/Model/User.swift
@@ -0,0 +1,23 @@
+//
+//  User.swift
+//  VK Mobile Challenge
+//
+//  Created by Ilya Gluschuk on 09/11/2018.
+//  Copyright © 2018 Ilya Glushchuk. All rights reserved.
+//
+
+import UIKit
+
+struct User: Decodable {
+    let id: Int
+    let firstName: String
+    let lastName: String
+    let photo: URL
+
+    enum CodingKeys: String, CodingKey {
+        case id
+        case firstName = "first_name"
+        case lastName = "last_name"
+        case photo = "photo_50"
+    }
+}
