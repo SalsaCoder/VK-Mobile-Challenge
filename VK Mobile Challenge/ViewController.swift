260bb954a8a72de163643300b6c9cace0e6dd301
Ilya Glushchuk <musicguy675@gmail.com>
2018-11-09 19:29:27 +1000

    initial commit

---
+++ VK Mobile Challenge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  VK Mobile Challenge
+//
+//  Created by Ilya Gluschuk on 09/11/2018.
+//  Copyright © 2018 Ilya Glushchuk. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
