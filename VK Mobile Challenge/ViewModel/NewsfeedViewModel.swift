1915be934ed1095ac767e60cbfb27591be976225
Ilya Glushchuk <musicguy675@gmail.com>
2018-11-10 16:40:00 +1000

    завел viewModel

---
+++ VK Mobile Challenge/ViewModel/NewsfeedViewModel.swift
@@ -0,0 +1,21 @@
+//
+//  NewsfeedViewModel.swift
+//  VK Mobile Challenge
+//
+//  Created by Ilya Gluschuk on 10/11/2018.
+//  Copyright © 2018 Ilya Glushchuk. All rights reserved.
+//
+
+import Foundation
+
+struct NewsfeedViewModel {
+    let name: String
+    let date: String
+    let authorImageUrl: URL
+    let text: String
+    let photoUrls: [URL]
+    let likeCount: Int
+    let commentsCount: Int
+    let repostCount: Int
+    let viewsCount: Int
+}
---
+++ VK Mobile Challenge/ViewModel/NewsfeedViewModelBuilder.swift
@@ -0,0 +1,45 @@
+//
+//  NewsfeedViewModelBuilder.swift
+//  VK Mobile Challenge
+//
+//  Created by Ilya Gluschuk on 10/11/2018.
+//  Copyright © 2018 Ilya Glushchuk. All rights reserved.
+//
+
+import UIKit
+
+final class NewsfeedViewModelBuilder {
+    let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "d MMM yyyy"
+
+        return dateFormatter
+    }()
+
+    func buildViewModels(from newsFeed: Newsfeed) -> [NewsfeedViewModel] {
+        var viewModels = [NewsfeedViewModel]()
+
+        for item in newsFeed.items {
+            guard let user = newsFeed.profiles.first(where: { $0.id == item.sourceId }) else {
+                continue
+            }
+
+            let name = "\(user.firstName) \(user.lastName)"
+            let date = dateFormatter.string(from: Date(timeIntervalSince1970: item.date))
+
+            let viewModel = NewsfeedViewModel(name: name,
+                                              date: date,
+                                              authorImageUrl: user.photo,
+                                              text: item.text,
+                                              photoUrls: [],
+                                              likeCount: item.likes.count,
+                                              commentsCount: item.comments.count,
+                                              repostCount: item.reposts.count,
+                                              viewsCount: item.views?.count ?? 0)
+
+            viewModels.append(viewModel)
+        }
+
+        return viewModels
+    }
+}
