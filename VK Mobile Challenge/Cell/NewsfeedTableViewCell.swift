406250442975ef25be0e9f2aed71771ad9951b78
Ilya Glushchuk <musicguy675@gmail.com>
2018-11-10 18:15:30 +1000

    добавил outlet  в ячейку

--- VK Mobile Challenge/Cell/NewsfeedTableViewCell.swift
+++ VK Mobile Challenge/Cell/NewsfeedTableViewCell.swift
@@ -10,4 +10,29 @@ import UIKit
 
 final class NewsfeedTableViewCell: UITableViewCell {
     static let reuseIdentifier = "NewsfeedTableViewCell"
+
+    @IBOutlet weak var profileImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    @IBOutlet weak var dateLabel: UILabel!
+    @IBOutlet weak var textView: UITextView!
+    @IBOutlet weak var showMoreButton: UIButton!
+    @IBOutlet weak var textViewHeightConstraint: NSLayoutConstraint!
+
+    @IBAction func tapShowMoreButton(_ sender: UIButton) {
+    }
+
+    func configure(with viewModel: NewsfeedViewModel) {
+        nameLabel.text = viewModel.name
+        dateLabel.text = viewModel.date
+        textView.text = viewModel.text
+
+        let sizeThatFitsTextView = textView.sizeThatFits(CGSize(width: textView.frame.size.width, height: CGFloat(MAXFLOAT)))
+        let heightOfText = sizeThatFitsTextView.height
+        textViewHeightConstraint.constant = heightOfText
+    }
+
+    override func prepareForReuse() {
+        super.prepareForReuse()
+        profileImageView.image = nil
+    }
 }
